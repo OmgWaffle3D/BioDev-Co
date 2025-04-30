4e04889ba2de4f389822a541166aefcd3a44b2f7
robertopeiro12 <robertopeiro12@gmail.com>
2025-04-30 11:12:47 -0600

    feat: update login page and add home page with new styles and functionality

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,12 +1,15 @@
 const login = () => {
-    if (username.value === "admin" && password.value === "1234") {
+    // Obtener los elementos por ID
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    if (email.value === "admin" && password.value === "1234") {
         alert("Bienvenido");
-        sessionStorage.name = username.value;
-        window.location = "./pages/profile.html";
+        sessionStorage.name = email.value;
+        // Corregir la ruta de redirección
+        window.location = "home.html";
     }
     else {
         alert("Usuario o contraseña incorrecta");
     }
-    
-    
 };
