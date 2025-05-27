0a7c2cb153d1e4c1d0a3c3cdcc3702f4fbed644e
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-05-27 09:18:40 -0600

    FrontEnd1

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,15 +1,34 @@
-const login = () => {
-    // Obtener los elementos por ID
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+// Este archivo contiene el código JavaScript para la página de inicio de sesión
+const boton = document.getElementById("btn-login");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 
-    if (email.value === "admin" && password.value === "1234") {
-        alert("Bienvenido");
-        sessionStorage.name = email.value;
-        // Corregir la ruta de redirección
-        window.location = "home.html";
+const  login =  async () => {
+    // validar credenciales
+    const credentials = {username:username.value, password: password.value}
+    const data = await fetch("http://localhost:5001/login", {
+        method:"POST",
+        headers:{"content-type":"application/json"}, 
+        body: JSON.stringify(credentials)});
+
+    const res = await data.json();
+    //console.log(username.value + " " + password.value);
+    // Si el login es correcto
+    if (res.isLogin) {
+        sessionStorage.setItem("name", res.user.name);
+        sessionStorage.setItem("id", res.user.id);
+        window.location = "./pages/profile.html";
+    }else {
+        // Si el login es incorrecto
+    alert("Credenciales incorrectas");
+
     }
-    else {
-        alert("Usuario o contraseña incorrecta");
-    }
+   
+    
+    
+
+    
 };
+
+
+boton.addEventListener("click", login);
