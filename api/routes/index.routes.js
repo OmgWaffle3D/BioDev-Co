9323099dfb9f15567713a3d29e75b8cdf1226154
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-05-28 12:12:29 -0600

    intento2

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -6,32 +6,6 @@ import {pool} from "../db/db.js"; // Import the database connection pool
 
 const router = Router();
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  pool.query(
-    "SELECT * FROM usuarios WHERE correo = ? AND contrasena = ?",
-    [username, password],
-    (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ isLogin: false, error: "Error en el servidor" });
-      }
-      if (results.length > 0) {
-        return res.json({
-          isLogin: true,
-          user: {
-            id: results[0].id,
-            name: results[0].nombre,
-            correo: results[0].correo
-          }
-        });
-      } else {
-        return res.json({ isLogin: false });
-      }
-    }
-  );
-});
-
 router.get("/registros", getRegistros);
 router.get("/usuarios", getUsuarios);
 router.post("/records", upload.array("images", 5), createRecord);
