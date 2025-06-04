108620b47823968a7907515455d9b708dd0dc6b0
robertopeiro12 <robertopeiro12@gmail.com>
2025-06-04 09:24:46 -0600

    Update API endpoint and key in chat controller; modify assistant cards in home page for navigation

--- api/controllers/chat.controllers.js
+++ api/controllers/chat.controllers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_ENDPOINT = process.env.API_ENDPOINT;
-const API_KEY = process.env.API_KEY;
+const API_ENDPOINT = "http://10.14.255.61/v1/chat/completions";
+const API_KEY = "sk-mDmOn2bG9Z3GDNW-x8wdeQ";
 
 export const getChatCompletion = async (req, res) => {
     try {
