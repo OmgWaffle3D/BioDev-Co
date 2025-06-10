9254aba4e5851e5d8afcad84ab81b92ea97e0b01
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-06-10 17:22:43 -0600

    admin uniforme y usuario uniforme

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,6 +1,21 @@
-// Add sidebar toggle functionality
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector(".toggle-btn")?.addEventListener("click", function() {
-        document.getElementById("sidebar").classList.toggle("collapsed");
+    const sidebarToggle = document.getElementById("sidebarToggle");
+    const sidebar = document.getElementById("sidebar");
+    const sidebarToggleIcon = document.getElementById("sidebarToggleIcon");
+
+    sidebarToggle?.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
+
+        if (sidebar.classList.contains("collapsed")) {
+            sidebarToggleIcon.textContent = "chevron_right";
+        } else {
+            sidebarToggleIcon.textContent = "chevron_left";
+        }
+    });
+
+    // Botón de mobile (menú hamburguesa)
+    const sidebarToggleMobile = document.getElementById("sidebarToggleMobile");
+    sidebarToggleMobile?.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
     });
 });
