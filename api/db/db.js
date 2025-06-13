8570e57c65a4a8d92acddd821db29106826532fb
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-06-12 19:30:36 -0600

    aiven

--- api/db/db.js
+++ api/db/db.js
@@ -1,11 +1,16 @@
-// Establece la conexion a la base de datos MySQL usando mysql2
 import mysql from "mysql2";
+import fs from "fs";
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "awaq_db2",
+  database: process.env.DB_DATABASE || "awaq_db",
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('./ca.pem') 
+  }
 });
 
 // For Debugging Purposes: Check if the connection is successful
